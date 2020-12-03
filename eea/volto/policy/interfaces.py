1458439eaa42b4170205b3bbdea9cf82e1004d4f
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-12-03 17:55:08 +0200

    Refs #121942 Fix import error

--- eea/volto/policy/interfaces.py
+++ eea/volto/policy/interfaces.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
+from eea.volto.policy import EEAMessageFactory as _
 from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
