d68e723e098e5c4c4716d9aa15c12ce82507d7ec
David Ichim <ichim.david@gmail.com>
2024-05-14 17:10:07 +0300

    lint fix

--- eea/volto/policy/restapi/serializer/summary.py
+++ eea/volto/policy/restapi/serializer/summary.py
@@ -2,8 +2,8 @@ from plone.restapi.interfaces import IJSONSummarySerializerMetadata
 from plone.restapi.serializer.summary import JSONSummarySerializerMetadata
 from zope.interface import implementer
 
+
 @implementer(IJSONSummarySerializerMetadata)
 class EEAJSONSummarySerializerMetadata(JSONSummarySerializerMetadata):
     def default_metadata_fields(self):
-        return {"@id", "@type", "description", "review_state", 
-        "title", "type_title", 'effective'}
+        return {"@id", "@type", "description", "review_state", "title", "type_title", 'effective'}
