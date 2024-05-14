505f9aadcd1e147144e357e066f9324da9485007
David Ichim <ichim.david@gmail.com>
2024-05-14 17:17:01 +0300

    lint fix for docstrings

--- eea/volto/policy/restapi/serializer/summary.py
+++ eea/volto/policy/restapi/serializer/summary.py
@@ -5,5 +5,25 @@ from zope.interface import implementer
 
 @implementer(IJSONSummarySerializerMetadata)
 class EEAJSONSummarySerializerMetadata(JSONSummarySerializerMetadata):
+    """
+    Summary Metadata serializer inherited within EEA Volto Policy.
+
+    Implements IJSONSummarySerializerMetadata interface.
+    """
+
     def default_metadata_fields(self):
-        return {"@id", "@type", "description", "review_state", "title", "type_title", 'effective'}
+        """
+        Returns a set of default metadata fields to be included in the summary.
+        We add effective date to the list of default fields.
+        Returns:
+            set: A set of metadata field names.
+        """
+        return {
+            "@id",
+            "@type",
+            "description",
+            "review_state",
+            "title",
+            "type_title",
+            "effective",
+        }
