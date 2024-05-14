12c9d0cca6a59506e8a4c867ffa0427d1eb84481
David Ichim <ichim.david@gmail.com>
2024-05-14 17:05:48 +0300

    feat(serializer): added effective to the default_metadata_fields exposed for JSONSummarySerializerMetadata
    - this is used by the parent_summary which is used by Volto to provide the
      metadata information to the content.parent object

---
+++ eea/volto/policy/restapi/serializer/summary.py
@@ -0,0 +1,9 @@
+from plone.restapi.interfaces import IJSONSummarySerializerMetadata
+from plone.restapi.serializer.summary import JSONSummarySerializerMetadata
+from zope.interface import implementer
+
+@implementer(IJSONSummarySerializerMetadata)
+class EEAJSONSummarySerializerMetadata(JSONSummarySerializerMetadata):
+    def default_metadata_fields(self):
+        return {"@id", "@type", "description", "review_state", 
+        "title", "type_title", 'effective'}
