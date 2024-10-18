47f3e61edaa461df84c9f9dbf3410d321f98afec
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-10-18 12:01:17 +0300

    fix: broken brain error at the upgrade svg step - refs #276995

--- eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
+++ eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
@@ -4,6 +4,7 @@ import logging
 import transaction
 from plone.namedfile.utils import getImageInfo
 from zope.lifecycleevent import modified
+from zope.annotation.interfaces import IAnnotations
 
 logger = logging.getLogger("upgrade_svgs")
 logger.setLevel(logging.INFO)
@@ -12,32 +13,48 @@ logger.setLevel(logging.INFO)
 def upgrade_svgs(portal):
     """Upgrade SVG dimensions"""
     i = 0
-    for brain in portal.portal_catalog():
-        obj = brain.getObject()
+    brains = portal.portal_catalog()
+    total = len(brains)
+    for idx, brain in enumerate(brains):
+        obj = dict()
+        try:
+            obj = brain.getObject()
+        except Exception:
+            continue  # Skip to the next item if there's an error
+
         if (
-            hasattr(obj, "image") and hasattr(obj.image, "_width") and
+            hasattr(obj, "image") and
+            hasattr(obj.image, "_width") and
             hasattr(obj.image, "_height")
         ):
-            logger.info("Processing %s", obj.absolute_url())
+
             contentType, width, height = getImageInfo(obj.image.data)
             if contentType == "image/svg+xml":
+                logger.info("Processing %s", obj.absolute_url())
                 obj.image._width = width
                 obj.image._height = height
-                modified(obj.image)
+                anno = IAnnotations(obj)
+                if "plone.scale" in anno:
+                    del anno["plone.scale"]
+                modified(obj)
                 i += 1
         if (
             hasattr(obj, "preview_image") and
             hasattr(obj.preview_image, "_width") and
             hasattr(obj.preview_image, "_height")
         ):
-            logger.info("Processing %s", obj.absolute_url())
             contentType, width, height = getImageInfo(obj.preview_image.data)
             if contentType == "image/svg+xml":
+                logger.info("Processing %s", obj.absolute_url())
                 obj.preview_image._width = width
                 obj.preview_image._height = height
-                modified(obj.preview_image)
+                anno = IAnnotations(obj)
+                if "plone.scale" in anno:
+                    del anno["plone.scale"]
+                modified(obj)
                 i += 1
         if not i % 100:
-            logger.info(i)
+            logger.warning("Progress %s/%s", idx, total)
+            logger.warning("Modified %s items", i)
             transaction.commit()
     transaction.commit()
