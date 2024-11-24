64e2b6e08c8a1835c4f46119757372062716c1be
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-11-25 01:35:46 +0200

    Improve svg upgrade step to know which svgs are fixed and not revisit them (#43)

--- eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
+++ eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
@@ -1,36 +1,35 @@
 """Upgrade step for svgs to fix width and height"""
-
-import logging
 import transaction
 from plone.namedfile.utils import getImageInfo
 from zope.lifecycleevent import modified
 from zope.annotation.interfaces import IAnnotations
-
-logger = logging.getLogger("upgrade_svgs")
-logger.setLevel(logging.INFO)
+from Products.ZCatalog.ProgressHandler import ZLogHandler
 
 
 def upgrade_svgs(portal):
     """Upgrade SVG dimensions"""
+
     i = 0
     brains = portal.portal_catalog()
-    total = len(brains)
+    pghandler = ZLogHandler(100)
+    pghandler.init("Recalculate svgs width and height", len(brains))
     for idx, brain in enumerate(brains):
+        pghandler.report(idx)
         obj = dict()
         try:
             obj = brain.getObject()
         except Exception:
             continue  # Skip to the next item if there's an error
 
+        # Process the main image only if dimensions are less than 5
         if (
             hasattr(obj, "image") and
             hasattr(obj.image, "_width") and
-            hasattr(obj.image, "_height")
+            hasattr(obj.image, "_height") and
+            (obj.image._width < 5 or obj.image._height < 5)
         ):
-
             contentType, width, height = getImageInfo(obj.image.data)
             if contentType == "image/svg+xml":
-                logger.info("Processing %s", obj.absolute_url())
                 obj.image._width = width
                 obj.image._height = height
                 anno = IAnnotations(obj)
@@ -38,14 +37,16 @@ def upgrade_svgs(portal):
                     del anno["plone.scale"]
                 modified(obj)
                 i += 1
+
+        # Process the preview image only if dimensions are less than 5
         if (
             hasattr(obj, "preview_image") and
             hasattr(obj.preview_image, "_width") and
-            hasattr(obj.preview_image, "_height")
+            hasattr(obj.preview_image, "_height") and
+            (obj.preview_image._width < 5 or obj.preview_image._height < 5)
         ):
             contentType, width, height = getImageInfo(obj.preview_image.data)
             if contentType == "image/svg+xml":
-                logger.info("Processing %s", obj.absolute_url())
                 obj.preview_image._width = width
                 obj.preview_image._height = height
                 anno = IAnnotations(obj)
@@ -53,8 +54,8 @@ def upgrade_svgs(portal):
                     del anno["plone.scale"]
                 modified(obj)
                 i += 1
+
         if not i % 100:
-            logger.warning("Progress %s/%s", idx, total)
-            logger.warning("Modified %s items", i)
             transaction.commit()
+    pghandler.finish()
     transaction.commit()
