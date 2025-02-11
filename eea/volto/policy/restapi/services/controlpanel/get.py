ae224620ac19b933b9ce68ebea71b0ac7c14dc5a
Miu Razvan <miu.razvan28@gmail.com>
2025-02-11 16:52:05 +0200

    update

--- eea/volto/policy/restapi/services/controlpanel/get.py
+++ eea/volto/policy/restapi/services/controlpanel/get.py
@@ -1,10 +1,13 @@
 """ Controlpanel get service """
 
+from zope.security import checkPermission
 from plone.restapi.services.controlpanels.get import (
     ControlpanelsGet as PloneControlpanelsGet
 )
 from eea.volto.policy.restapi.services.controlpanel.utils import (
-    has_controlpanel_permission
+    has_controlpanel_permission,
+    get_permission,
+    send_unauthorized
 )
 
 
@@ -15,6 +18,10 @@ class ControlpanelsGet(PloneControlpanelsGet):
         """ Reply """
         if self.params:
             return self.reply_panel()
+        permission = get_permission("plone.app.controlpanel.Overview")
+        has_permission = checkPermission(permission.id, self.context)
+        if not has_permission:
+            send_unauthorized(self.context, self.request)
         return super().reply()
 
     def reply_panel(self):
