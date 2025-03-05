dfd6f0d05efbc62ab37f8593213e8fedc9d63f38
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2025-03-05 12:37:54 +0200

    fix: Make upgrade step for svg update more efficent - refs #276995

--- eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
+++ eea/volto/policy/upgrades/upgrade_svgs/upgrade_svg.py
@@ -1,4 +1,5 @@
 """Upgrade step for svgs to fix width and height"""
+
 import transaction
 from plone.namedfile.utils import getImageInfo
 from zope.lifecycleevent import modified
@@ -6,56 +7,56 @@ from zope.annotation.interfaces import IAnnotations
 from Products.ZCatalog.ProgressHandler import ZLogHandler
 
 
-def upgrade_svgs(portal):
-    """Upgrade SVG dimensions"""
+def process_image(obj, image_attr):
+    """Process an image field if it's an SVG and needs updating."""
+    image = getattr(obj, image_attr, None)
+    if (
+        image and
+        hasattr(image, "_width") and
+        hasattr(image, "_height") and
+        (image._width < 5 or image._height < 5)  # Skip already upgraded images
+    ):
+        contentType, width, height = getImageInfo(image.data)
+        if contentType == "image/svg+xml":
+            image._width = width
+            image._height = height
+            anno = IAnnotations(obj)
+            if "plone.scale" in anno:
+                del anno["plone.scale"]
+            modified(obj)
+            return True
+    return False
 
-    i = 0
+
+def upgrade_svgs(portal):
+    """Upgrade SVG dimensions with intermediate commits"""
+
     brains = portal.portal_catalog()
+    batch_size = 30
+    total_brains = len(brains)
     pghandler = ZLogHandler(100)
-    pghandler.init("Recalculate svgs width and height", len(brains))
+    pghandler.init("Recalculate svgs width and height", total_brains)
+
+    updated_count = 0  # Tracks number of updates
     for idx, brain in enumerate(brains):
-        pghandler.report(idx)
         obj = dict()
+        pghandler.report(idx)
         try:
             obj = brain.getObject()
         except Exception:
-            continue  # Skip to the next item if there's an error
+            continue  # Skip object if it cannot be retrieved
 
-        # Process the main image only if dimensions are less than 5
-        if (
-            hasattr(obj, "image") and
-            hasattr(obj.image, "_width") and
-            hasattr(obj.image, "_height") and
-            (obj.image._width < 5 or obj.image._height < 5)
-        ):
-            contentType, width, height = getImageInfo(obj.image.data)
-            if contentType == "image/svg+xml":
-                obj.image._width = width
-                obj.image._height = height
-                anno = IAnnotations(obj)
-                if "plone.scale" in anno:
-                    del anno["plone.scale"]
-                modified(obj)
-                i += 1
+        # Process both main and preview images
+        updated = (
+                process_image(obj, "image") or
+                process_image(obj, "preview_image")
+        )
+        if updated:
+            updated_count += 1
 
-        # Process the preview image only if dimensions are less than 5
-        if (
-            hasattr(obj, "preview_image") and
-            hasattr(obj.preview_image, "_width") and
-            hasattr(obj.preview_image, "_height") and
-            (obj.preview_image._width < 5 or obj.preview_image._height < 5)
-        ):
-            contentType, width, height = getImageInfo(obj.preview_image.data)
-            if contentType == "image/svg+xml":
-                obj.preview_image._width = width
-                obj.preview_image._height = height
-                anno = IAnnotations(obj)
-                if "plone.scale" in anno:
-                    del anno["plone.scale"]
-                modified(obj)
-                i += 1
-
-        if not i % 100:
+        # Commit transaction every batch_size updates
+        if updated_count and updated_count % batch_size == 0:
             transaction.commit()
+
     pghandler.finish()
     transaction.commit()
